b91523f00aee98ccf75118ec33c5d5420b5f74c7
charles <93201255+r4Ggit@users.noreply.github.com>
2022-01-17 16:04:55 +0800

    Update Main.java

--- q3/src/com/company/Main.java
+++ q3/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int x = 102; x > 65; x = x - 1) {
+        for (int x = 101; x > 66; x = x - 2) {
             System.out.print("value of x : " + x);
             System.out.print("\n");
         }
