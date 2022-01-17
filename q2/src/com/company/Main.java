a42037f0043e023898616c86bf86ef0bb3071b56
charles <93201255+r4Ggit@users.noreply.github.com>
2022-01-17 16:10:01 +0800

    Update Main.java

--- q2/src/com/company/Main.java
+++ q2/src/com/company/Main.java
@@ -5,23 +5,26 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
             {
-                String modulecode = "1009";
+                String modulecode = "CSC1009";
                 switch(modulecode)
                 {
-                    case "1007":
+		    case "CSC1006":
+			System.out.println("Mathematics 2");
+			break;
+                    case "CSC1007":
                         System.out.println("operating systems");
                         break;
-                    case "1008":
+                    case "CSC1008":
                         System.out.println("data structure and algorithms");
                         break;
-                    case "1009":
+                    case "CSC1009":
                         System.out.println("object oriented programming");
                         break;
-                    case "1010":
+                    case "CSC1010":
                         System.out.println("computer networks");
                         break;
                     default:
-                        System.out.println("mathematics");
+                        System.out.println("error! no module found");
                         break;
                 }
             }
