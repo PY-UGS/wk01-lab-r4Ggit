77c64e0ade2201942be53b0262542684d21eabe8
charles <93201255+r4Ggit@users.noreply.github.com>
2022-01-17 15:01:36 +0800

    Add files via upload

---
+++ q2/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+            {
+                String modulecode = "1009";
+                switch(modulecode)
+                {
+                    case "1007":
+                        System.out.println("operating systems");
+                        break;
+                    case "1008":
+                        System.out.println("data structure and algorithms");
+                        break;
+                    case "1009":
+                        System.out.println("object oriented programming");
+                        break;
+                    case "1010":
+                        System.out.println("computer networks");
+                        break;
+                    default:
+                        System.out.println("mathematics");
+                        break;
+                }
+            }
+        }
+    }
+}
